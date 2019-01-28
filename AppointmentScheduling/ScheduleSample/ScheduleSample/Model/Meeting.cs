b643f943087e1048318771fdd63fa3b494632d9c
Thangapriya V <thangapriya@syncfusion.com>
2019-01-28 15:05:24 +0530

    Appointment scheduling sample is added.

---
+++ AppointmentScheduling/ScheduleSample/ScheduleSample/Model/Meeting.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleSample
+{
+    public class Meeting : INotifyPropertyChanged
+    {
+        #region private variables
+
+        private string eventName;
+        private string organizer;
+        private DateTime beginTime;
+        private DateTime endTime;
+        private Color color;
+        private bool isAllDay;
+        private string recurrenceRule;
+
+
+        #endregion
+
+        #region Public Properties
+        public string EventName
+        {
+            get
+            {
+                return this.eventName;
+            }
+            set
+            {
+                this.eventName = value;
+                this.RaisePropertyChanged("EventName");
+            }
+        }
+        public string Organizer
+        {
+            get
+            {
+                return this.organizer;
+            }
+            set
+            {
+                this.organizer = value;
+                this.RaisePropertyChanged("Organizer");
+            }
+        }
+        public DateTime BeginTime
+        {
+            get
+            {
+                return this.beginTime;
+            }
+            set
+            {
+                this.beginTime = value;
+                this.RaisePropertyChanged("BeginTime");
+            }
+        }
+        public DateTime EndTime
+        {
+            get
+            {
+                return this.endTime;
+            }
+            set
+            {
+                this.endTime = value;
+                this.RaisePropertyChanged("EndTime");
+            }
+        }
+        public Color Color
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                this.color = value;
+                this.RaisePropertyChanged("Color");
+            }
+        }
+        public bool IsAllDay
+        {
+            get
+            {
+                return this.isAllDay;
+            }
+            set
+            {
+                this.isAllDay = true;
+                this.RaisePropertyChanged("IsAllDay");
+            }
+        }
+        public string RecurrenceRule
+        {
+            get
+            {
+                return this.recurrenceRule;
+            }
+            set
+            {
+                this.recurrenceRule = value;
+                this.RaisePropertyChanged("RecurrenceRule");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ AppointmentScheduling/ScheduleSample/ScheduleSample/ViewModel/ViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleSample
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Meeting> Meetings { get; set; }
+
+        List<string> eventNameCollection;
+        List<Color> colorCollection;
+        public ViewModel()
+        {
+            this.Meetings = new ObservableCollection<Meeting>();
+            this.CreateEventNameCollection();
+            this.CreateColorCollection();
+            this.CreateAppointments();
+        }
+
+        /// <summary>
+        /// Creates meetings and stores in a collection.  
+        /// </summary>
+        private void CreateAppointments()
+        {
+            Random randomTime = new Random();
+            List<Point> randomTimeCollection = GetTimeRanges();
+            DateTime date;
+            DateTime DateFrom = DateTime.Now.AddDays(-10);
+            DateTime DateTo = DateTime.Now.AddDays(10);
+            DateTime dataRangeStart = DateTime.Now.AddDays(-3);
+            DateTime dataRangeEnd = DateTime.Now.AddDays(3);
+
+            for (date = DateFrom; date < DateTo; date = date.AddDays(1))
+            {
+                if ((DateTime.Compare(date, dataRangeStart) > 0) && (DateTime.Compare(date, dataRangeEnd) < 0))
+                {
+                    for (int appointmentIndex = 0; appointmentIndex < 3; appointmentIndex++)
+                    {
+                        Meeting meeting = new Meeting();
+                        int hour = (randomTime.Next((int)randomTimeCollection[appointmentIndex].X, (int)randomTimeCollection[appointmentIndex].Y));
+                        meeting.BeginTime = new DateTime(date.Year, date.Month, date.Day, hour, 0, 0);
+                        meeting.EndTime = (meeting.BeginTime.AddHours(1));
+                        meeting.EventName = eventNameCollection[randomTime.Next(4)];
+                        meeting.Color = colorCollection[randomTime.Next(3)];
+                        if (appointmentIndex % 3 == 0)
+                            meeting.IsAllDay = true;
+                        Meetings.Add(meeting);
+                    }
+                }
+                else
+                {
+                    Meeting meeting = new Meeting();
+                    meeting.BeginTime = new DateTime(date.Year, date.Month, date.Day, randomTime.Next(9, 11), 0, 0);
+                    meeting.EndTime = (meeting.BeginTime.AddHours(1));
+                    meeting.EventName = eventNameCollection[randomTime.Next(4)];
+                    meeting.Color = colorCollection[randomTime.Next(3)];
+                    Meetings.Add(meeting);
+                }
+            }
+         
+            this.Meetings[1].RecurrenceRule = "FREQ=DAILY;INTERVAL=1;COUNT=5";
+        }
+
+        /// <summary>  
+        /// Creates event names collection.  
+        /// </summary>  
+        private void CreateEventNameCollection()
+        {
+            eventNameCollection = new List<string>();           
+
+            eventNameCollection.Add("Meeting");
+            eventNameCollection.Add("Birthday");
+            eventNameCollection.Add("Checkup");
+            eventNameCollection.Add("Conference");
+        }
+
+        /// <summary>  
+        /// Creates color collection.  
+        /// </summary>  
+        private void CreateColorCollection()
+        {
+            colorCollection = new List<Color>();
+            colorCollection.Add(Color.FromHex("#FF339933"));
+            colorCollection.Add(Color.FromHex("#FF00ABA9"));
+            colorCollection.Add(Color.FromHex("#FFE671B8"));
+        }
+
+        /// <summary>
+        /// Gets the time ranges.
+        /// </summary>
+        private List<Point> GetTimeRanges()
+        {
+            List<Point> randomTimeCollection = new List<Point>();
+            randomTimeCollection.Add(new Point(9, 11));
+            randomTimeCollection.Add(new Point(12, 14));
+            randomTimeCollection.Add(new Point(15, 17));
+            return randomTimeCollection;
+        }
+    }
+}
